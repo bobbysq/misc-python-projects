e548c5f6f58072d8f1b2d3dd94eeb559552f0181
bobbysq <parsonsrob@uhs614-11-19020-lab.local>
2015-11-30 13:33:12 -0600

    added a period and a readme

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import time
 
 name = raw_input("What's your name? ")
-print("Hello, "+name)
+print("Hello, "+name+".")
 time.sleep(1)
 print("Made with nano")
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is a bunch of random programs I'm making in CSE. Some of them are assignments, a lot of them aren't.
